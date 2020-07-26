3064733e0f0290d1690434072b03790cdfe6c4a5
DK <lovemybabyxuan@hotmail.com>
2020-07-26 16:53:17 +0800

    2020-07-26练习

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import unittest
 from testcases.test_fixture import TestFixture
 from testcases.test_demo import TestStringMethods
+from testcases.test_filehandler import TestFileHandler
 # def creat_suite():
 #
 #     suite = unittest.TestSuite()
@@ -13,11 +14,12 @@ def creat_suite_CI():
     loader = unittest.TestLoader()
     tests = loader.loadTestsFromTestCase(TestFixture)
     tests_02 = loader.loadTestsFromTestCase(TestStringMethods)
+    tests_03 = loader.loadTestsFromTestCase(TestFileHandler)
     # print(tests)
     # suite.addTests([TestFixture('test_01'),
     #                TestFixture('test_02'),
     #                TestFixture('test_03')])
-    suite.addTests([tests,tests_02])
+    suite.addTests([tests,tests_02,tests_03])
     return suite
 
 if __name__ == '__main__':
--- tools/filehandler.py
+++ tools/filehandler.py
@@ -1,27 +1,49 @@
-import random
+"""
+文件处理类，在这个文件处理类型添加对csv文件，excel文件处理的方法
+"""
 import csv
+import os
+data_dir = os.path.join(os.path.dirname(__file__),'../data')
+if not os.path.exists(data_dir):
+    os.mkdir(data_dir)
 
-def random_phone():
-    phone_frefix = ['13','14','15','16','17','18','19']
-    phone_last = ''
-    for x in range(9):
-        # 生成随机的整数
-        phone_last += str(random.randrange(10))
+class FileHandler:
+    def __init__(self,filename):
+        self.filepath = os.path.join(data_dir, filename)
 
-    return random.choices(phone_frefix)[0]+phone_last
+    def readcsv(self):
+        """
+        从csv文件中读取数据
+        :return: reader
+        """
+        if not os.path.exists(self.filepath):
+            return None
+        data=[]
+        with open(self.filepath, mode='r', encoding='utf8') as f:
+            reader = csv.reader(f)
+            for line in reader:
+                data.append(line)
+        return data
+
+
+    def writecsv(self,listdata):
+        """
+        写入数据到csv文件中
+        :param listdata: 写入文件的数据 [[],[]] 二维的list
+        :return:
+        """
+        # 如果这个文件所在的目录不存在，创建目录
+        if not os.path.exists(os.path.dirname(self.filepath)):
+            # TODO 创建多级目录
+            os.makedirs(os.path.dirname(self.filepath))
+            # os.mkdir(os.path.dirname(self.filepath))
+
+        with open(self.filepath, mode='w', encoding='utf8', newline='') as f:
+            writer = csv.writer(f)
+            # 需要使用到list 数据
+            for line in listdata:
+                writer.writerow(line)
 
 if __name__ == '__main__':
-    with open('phone.csv',mode='w',encoding='utf8',newline='') as f:
-        headers = ['编号','手机号']
-        writer = csv.DictWriter(f,fieldnames=headers)
-        writer.writeheader()
-
-        phone_list = []
-        counter = 0
-        while counter<100:
-            num = random_phone()
-            if not num in phone_list:
-                # f.write(num+"\n")
-                writer.writerow({'编号':counter,'手机号':num})
-                phone_list.append(num)
-                counter+=1
+    file = FileHandler('data.csv')
+    file.writecsv([['id', 'name'], ['1', 'xiaozhang'], ['2', 'xiaoming']])
