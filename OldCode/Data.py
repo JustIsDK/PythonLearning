1b37241a003a1707132193c7833d69c37cfd678f
DK <lovemybabyxuan@hotmail.com>
2021-03-27 15:04:18 +0800

    11

--- OldCode/Data.py
+++ OldCode/Data.py
@@ -3,7 +3,7 @@ from faker import Faker
 f = Faker(locale='zh_CN')
 
 for i in range(0,28):
-     print(f.name()+"自")
+     print(f.name()+"X")
     # print(f.ssn())
     # print(f.email())
     # print(f.ssn(min_age=18, max_age=45, gender=None))
