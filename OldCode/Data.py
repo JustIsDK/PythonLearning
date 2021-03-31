d19f5eb8ca8fc6a8002b2307b6fa8c9d2b26433e
DK <lovemybabyxuan@hotmail.com>
2021-03-31 21:04:40 +0800

    去重算法 非set实现

--- OldCode/Data.py
+++ OldCode/Data.py
@@ -7,3 +7,17 @@ for i in range(0,28):
     # print(f.ssn())
     # print(f.email())
     # print(f.ssn(min_age=18, max_age=45, gender=None))
+
+'''
+去重算法
+'''
+a = ['a','b','b','c','c','d','e','e']
+b = []
+for i in a:
+     if i in b:
+          pass
+     else:
+          b.append(i)
+
+print(b)
+
