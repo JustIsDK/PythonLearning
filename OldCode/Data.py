152fe83f20ad65808fbc8bd920bac7175237cd98
huakai <395522344@qq.com>
2021-04-09 13:29:31 +0800

    日常维护

--- OldCode/Data.py
+++ OldCode/Data.py
@@ -2,8 +2,12 @@ from faker import Faker
 
 f = Faker(locale='zh_CN')
 
-for i in range(0,28):
-     print(f.name()+"自")
-    # print(f.ssn())
-    # print(f.email())
-    # print(f.ssn(min_age=18, max_age=45, gender=None))
+for i in range(0,7):
+     print(f.name()+"静")
+#     # print(f.ssn())
+#     # print(f.email())
+#     # print(f.ssn(min_age=18, max_age=45, gender=None))
+#
+#
+# for i in range(0,5):
+#      print('肥宅！！')
