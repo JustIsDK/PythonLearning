b0ceafef9b1943e40f7092565d5339d519fda335
lovemybabyxuan@hotmail.com <120515@na>
2021-06-16 11:32:10 +0800

    测试

--- FM/Demo.py
+++ FM/Demo.py
@@ -73,11 +73,11 @@
 # print(num2)
 
 
-def f(a,l=[]):
-    l.append(a)
-    return l
-    print(l)
-
-print(f(1))
-print(f(2))
-print(f(3))
+# def f(a,l=[]):
+#     l.append(a)
+#     return l
+#     print(l)
+#
+# print(f(1))
+# print(f(2))
+# print(f(3))
