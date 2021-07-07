ac6cfea2fe3cab5e41df415a8dee1ab350953bca
lovemybabyxuan <120515@na>
2021-07-07 17:31:35 +0800

    PytestDemo 练习

--- FM/PytestDemo1.py
+++ FM/PytestDemo1.py
@@ -3,13 +3,16 @@ import pytest
 import PytestDemo
 '''这个是引入另一个文件中的方法，那个文件中的是被测方法'''
 
-def test_max():
+@pytest.mark.parametrize("data,expection",[((1,2,3,4,5),5),((2,3,4,5,6),6)])
+
+
+def test_max(data,expection):
     '''这里写的是测试用被测方法的方法'''
-    value = (2,1,3,5,2,7)
+    # value = (2,1,3,5,2,7)
     '''这里赋值给待测方法'''
-    val = PytestDemo.max(value)
+    val = PytestDemo.max(data)
     '''把被测方法的值赋值给val'''
-    assert val == 7
+    assert val == expection
     '''做断言'''
 @pytest.mark.skip
 def test_min():
