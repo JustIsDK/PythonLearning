430a70d912db9ccb40c8e03d62680eb9dffeaa4a
lovemybabyxuan <120515@na>
2021-09-03 17:56:32 +0800

    接口自动化req库使用 到post章节

---
+++ Learning/DemoDDT.py
@@ -0,0 +1,11 @@
+import unittest
+from ddt import ddt,data
+
+# @ddt
+# class TestDDT(unittest.TestCase):
+#     @data(10,5,4,8)
+#     def test_datas(self,test_value):
+#         print('value is ',test_value)
+#         self.assertGreater(test_value,1)
+
+
