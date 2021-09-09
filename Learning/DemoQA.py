686dae0ffaf6834293f1b5dca9efb0f6191eebd5
DK <lovemybabyxuan@hotmail.com>
2021-09-09 16:39:55 +0800

    QA获取Token

---
+++ Learning/DemoQA.py
@@ -0,0 +1,21 @@
+import unittest
+import requests
+
+class GetToken(unittest.TestCase):
+    def get_qa_token(self):
+        get_header = {
+            'origin': 'https://uat-ecp-site.adidas.com.cn',
+            'referer': 'https://uat-ecp-site.adidas.com.cn/',
+            'source': 'A001',
+            'Content-Type':'application/x-www-form-urlencoded'
+        }
+        get_post = {
+            'smsOtp': '000000',
+            'phone': '18437909853'
+        }
+        url = 'https://sit-auth-api.adidas.com.cn/v1/users/login/sms'
+
+        r = requests.post(url=url,data=get_post,headers=get_header)
+        print(r.json())
+        # accesstoken = r.json()[data.accesstoken]
+        # print(accesstoken)
