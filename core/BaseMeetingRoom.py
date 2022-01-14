8b77cb82ec20a22f82bd2848f069381b0a63461c
sh00013ml <lovemybabyxuan@hotmail.com>
2022-01-14 16:29:05 +0800

    删除老代码，不破不立

--- core/BaseMeetingRoom.py
+++
@@ -1,6 +0,0 @@
-from core.BaseClass import BaseCase
-from config import config_data
-class BaseMettingRoom(BaseCase):
-    @classmethod
-    def setUpClass(cls) -> None:
-        cls.token = cls.get_token(secret=config_data['secret']['meetingroomsecret'])
--- testcases/test_oa.py
+++
@@ -1,7 +0,0 @@
-from core.base_oa import BaseOA
-class TestOA(BaseOA):
-    def test_a(self):
-        print(self.token)
-
-    def test_b(self):
-        print(self.token)
