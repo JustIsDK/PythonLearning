0f9557a05787958e5baea3922a750c98db3c34aa
sh00013ml <lovemybabyxuan@hotmail.com>
2022-01-14 16:29:05 +0800

    删除老代码，不破不立

--- testcases/test_meetingroom.py
+++
@@ -1,8 +0,0 @@
-from core.BaseMeetingRoom import BaseMettingRoom
-class TestMeetingroom(BaseMettingRoom):
-    def test_1(self):
-        print(self.token)
-
-
-    def test_2(self):
-        print(self.token)
