2a4d05dbaceb198203892f59e60fb50f53e247f5
lovemybabyxuan <395522344@qq.com>
2022-04-02 21:57:38 +0800

    字符串处理

--- OldCode/Temp.py
+++ OldCode/Temp.py
@@ -1,3 +1,17 @@
+a = 'pid={商户ID}&type={支付方式}&out_trade_no={商户订单号}&notify_url={服务器异步通知地址}&return_url={页面跳转通知地址}&name={商品名称}&money={金额}&sitename={网站名称}&sign={签名字符串}&sign_type=MD5'
+
+# a = 'act=order&pid={商户ID}&key={商户密钥}&out_trade_no={商户订单号}'
+b = a.split('&')
+print('{')
+for i in b:
+     d = i.split('=')
+     print('"'+d[0]+'"'+':'+'"'+d[1]+'"'+',')
+
+print('}')
+# import random
+#
+# a = random.randint(00000000000000000,99999999999999999)
+# print(a)
 import time
 
 now = time.time()
