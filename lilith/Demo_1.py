d243280b7e5b980b94d53ee80d4edcfec3e573ae
lilith <395522344@qq.com>
2023-02-28 19:06:55 +0800

    11

--- lilith/Demo_1.py
+++ lilith/Demo_1.py
@@ -46,5 +46,31 @@ def fib2(n):
         a,b = b,a+b
     print(result)
 
-fib(100)
-fib2(100)
+def is_unique(string):
+    if string is None:
+        return False
+    return len(set(string))==len(string)
+
+
+'''
+sort() 和 sorted() 的区别：sort() 应用在 list 列表中
+而 sorted() 可以对所有可迭代的对象进行排序操作
+'''
+
+def is_queue(str1,str2):
+    if str1 is None or str2 is None:
+        return False
+    return sorted(str1)==sorted(str2)
+
+
+def plusOne(digits:list[int]) -> list[int]:
+    num = ''
+    for i in digits:
+        num  = num + str(i)
+    newlist = int(num) + 1
+    newlist = str(newlist)
+    newlist2 = []
+    for i in newlist:
+        newlist2.append(i)
+    print(newlist2)
+
